d86e12ded7c7ff1dcbc1acd24417aecd858782cd
jef4490 <jeffrey.hechler@gmail.com>
2017-02-27 17:59:43 -0500

    adjust db

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,22 @@ Bundler.require
 
 require_all('app/')
 
-set(:database, {adapter: "sqlite3", database: "workshop.sqlite3"})
+configure :development do
+  set :database, "sqlite3:///database.db"
+end
+
+configure :production do
+  db = URI.parse(ENV['devej5lsgcubdc'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+      )
+
+end
+
+# set(:database, {adapter: "sqlite3", database: "workshop.sqlite3"})
